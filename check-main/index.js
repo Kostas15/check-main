7283d22e1b041d72c47f45e4ffc5ba8a65bb14ad
Kostas15 <kostasfas1999@gmail.com>
2023-10-16 20:55:48 +0300

    form

---
+++ check-main/index.js
@@ -0,0 +1,7 @@
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const newDiv = document.getElementById("add")
+
+function func() {
+    newDiv.innerHTML = email + password
+}
